f12baba4ead51e353a31d0568d150acbac05c158
jtrfid <jtrfid@qq.com>
2016-07-28 17:26:07 +0800

    Notes：集合中随机选一个元素

--- src/de/invation/code/toval/misc/valuegeneration/RandomChooser.java
+++ src/de/invation/code/toval/misc/valuegeneration/RandomChooser.java
@@ -6,11 +6,18 @@ import java.util.Random;
 import de.invation.code.toval.validate.ParameterException;
 import de.invation.code.toval.validate.Validate;
 
-
+/**
+ * 在集合中，随机选一个元素
+ *
+ * @param <E> 集合元素类型
+ */
 public class RandomChooser<E extends Object> implements ValueChooser<E> {
 	
 	Random rand = new Random();
-
+    
+	/**
+	 * 在参数集合中，随机选一个元素
+	 */
 	@Override
 	public E chooseValue(List<E> candidates) throws ParameterException {
 		Validate.notNull(candidates);
@@ -19,6 +26,9 @@ public class RandomChooser<E extends Object> implements ValueChooser<E> {
 		return candidates.get(rand.nextInt(candidates.size()));
 	}
 
+	/**
+	 * 是否有效
+	 */
 	@Override
 	public boolean isValid() {
 		return true;
--- src/de/invation/code/toval/misc/valuegeneration/ValueChooser.java
+++ src/de/invation/code/toval/misc/valuegeneration/ValueChooser.java
@@ -4,11 +4,24 @@ import java.util.List;
 
 import de.invation.code.toval.validate.ParameterException;
 
-
+/**
+ * 集合中选一个元素
+ *
+ * @param <E> 集合元素类型
+ */
 public interface ValueChooser<E extends Object> {
-	
+	/**
+	 * 参数集合中选一个值
+	 * @param candidates
+	 * @return
+	 * @throws ParameterException
+	 */
 	public E chooseValue(List<E> candidates) throws ParameterException;
 	
+	/**
+	 * 是否有效
+	 * @return
+	 */
 	public boolean isValid();
 
 }
